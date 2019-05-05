dae43f7964569455817db4eef1564f13520c411f
RJ <jensenrs@gmail.com>
2019-05-05 12:51:50 -0700

    Product as Component

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import mockProducts from '../mockdata.json'
 
 const Product = ({}) => {
   return (
     <div>
-      <p>product</p>
+      { mockProducts.map((el, i) => {
+        return (
+          <div key={`${i}-product`} className='product-container flex-center'>
+
+            <div className='flex-center'>
+              <div>
+                <img
+                  src={require(`../images/${el.image}`)}
+                  alt={el.name}
+                  className='product-image'
+                ></img>
+              </div>
+              <div className='name-sku-box'>
+                <h3>{el.name}</h3>
+                <p>{el.sku}</p>
+              </div>
+            </div>
+
+            <div>
+              <p>qty: {el.quantity}</p>
+            </div>
+
+            <div>
+              <p>price: {el.unitPrice}</p>
+            </div>
+
+          </div>
+        )
+      })}
     </div>
   )
 }
--- src/Containers/Cart.js
+++ src/Containers/Cart.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-// import Product from '../Components/Product'
-import mockProducts from '../mockdata.json'
+import Product from '../Components/Product'
 
 class Cart extends Component {
   constructor(props) {
@@ -11,37 +10,7 @@ class Cart extends Component {
 
   render () {
     return (
-      <div>
-        { mockProducts.map((el, i) => {
-          return (
-            <div key={`${i}-product`} className='product-container flex-center'>
-
-              <div className='flex-center'>
-                <div>
-                  <img
-                    src={require(`../images/${el.image}`)}
-                    alt={el.name}
-                    className='product-image'
-                  ></img>
-                </div>
-                <div className='name-sku-box'>
-                  <h3>{el.name}</h3>
-                  <p>{el.sku}</p>
-                </div>
-              </div>
-
-              <div>
-                <p>qty: {el.quantity}</p>
-              </div>
-
-              <div>
-                <p>price: {el.unitPrice}</p>
-              </div>
-
-            </div>
-          )
-        })}
-      </div>
+      <Product />
     )
   }
 }
